f4455b1eb6621f9ded52d62f12e8ae04b877af7b
Tiffany Zhang <zhangtiff@google.com>
2021-01-02 14:55:17 -0500

    Add trivial linear dungeon generation.

--- src/App.tsx
+++ src/App.tsx
@@ -79,24 +79,116 @@ function isWalkable(c: Coordinates, floor: DungeonFloor): boolean {
   return key in floor && floor[key];
 }
 
+/**
+ * Generates a random number between a max and minimum.
+ * @param max Highest possible number that can be generated, inclusive.
+ * @param min Optional minimum number, inclusive. 
+ */
+function randomInt(max: number, min = 0): number {
+  return min + Math.floor(Math.random() * (max + 1 - min));
+}
+
+/**
+ * Returns true or false randomly.
+ * @returns true if heads, false if tails.
+ */
+function coinFlip(): boolean {
+  return Math.random() > 0.5;
+}
+
 const cachedLevels: {[index:number]: LevelPlan} = {};
+/**
+ * Generates a single floor of the dungeon.
+ * @param num What floor number the player is on.
+ * @returns Data for the level the player can interact with.
+ */
 function generateLevel(num: number): LevelPlan {
   // Avoid regenerating a level if we've already done it.
   if (num in cachedLevels) return cachedLevels[num];
 
-  const extraTiles: DungeonFloor = {};
+  const floor: DungeonFloor = {};
+  let player: Coordinates = {x: 0, y: 0};
+  let ladder: Coordinates = {x: 0, y: 0};
 
+  const center: Coordinates = {x: 0, y: 0};
+  let leftEntrance: boolean = true;
+
+  // Generate one room for each level the player is on.
   for (let i = 0; i < num; i++) {
-    extraTiles[tileKey({x: 2 + i, y: 1})] = true;
+    const roomSize: Dimensions = {
+      height: randomInt(10, 4),
+      width: randomInt(10, 4),
+    };
+
+    const relativeCenter: Coordinates = {
+      x: Math.floor(roomSize.width / 2),
+      y: Math.floor(roomSize.height / 2),
+    };
+
+    if (leftEntrance) {
+      center.x += relativeCenter.x;
+    } else {
+      center.y += relativeCenter.y;
+    }
+
+    // Draw the room.
+    const xStart = center.x - relativeCenter.x;
+    const xEnd = xStart + roomSize.width;
+    const yStart = center.y - relativeCenter.y;
+    const yEnd = yStart + roomSize.height;
+    
+    for (let y = yStart; y < yEnd; y++) {
+      for (let x = xStart; x < xEnd; x++) {
+        // Add walkable path.
+        floor[tileKey({x, y})] = true;
+      }
+    }
+
+    // Add additional room features.
+    if (i === 0) {
+      // Place the player at the beginning of the path.
+      player = {...center};
+    }
+    if (i === num - 1) {
+      // Place the ladder at the end of the path. 
+      ladder = {
+        // Make the ladder not collide with the player when there's one room.
+        x: center.x + 1,
+        y: center.y,
+      };
+    } else {
+      // Add a corridor only if we're not at the end.
+      const corridorLength = randomInt(5, 2);
+
+      leftEntrance = coinFlip();
+
+      if (leftEntrance) {
+        // Go right if heads.
+        const xOffset = center.x + (roomSize.width - relativeCenter.x);
+        const y = center.y;
+
+        let x;
+        for (x = xOffset; x < xOffset + corridorLength; x++) {
+          floor[tileKey({x, y})] = true;
+        }
+
+        center.x = x;
+      } else {
+        // Otherwise go down.
+        const yOffset = center.y + (roomSize.height - relativeCenter.y);
+        const x = center.x;
+
+        let y;
+        for (y = yOffset; y < yOffset + corridorLength; y++) {
+          floor[tileKey({x, y})] = true;
+        }
+
+        center.y = y;
+      }
+    }
+
   }
-
-  const nextLevel: LevelPlan = {
-    floor: {...SAMPLE_FLOOR, ...extraTiles},
-    player: {...SAMPLE_PLAYER_LOCATION},
-    ladder: {...SAMPLE_LADDER_LOCATION},
-  };
-
-  return nextLevel;
+  return {floor, player, ladder};
 }
 
 export default function App() {
@@ -189,10 +281,10 @@ function DungeonView({floor, player, viewport, tileSize, ladder}:
             borderRadius: tileBorderRadius(tile, floor),
             width: tileSize.width,
             height: tileSize.height,
-            backgroundColor: isPath ? '#eee' : 'none',
+            backgroundColor: isPath ? '#eee' : 'transparent',
           }}
         >
-          {DEBUG ? <span className={styles.debugCoordinates}>{key}</span> : ''}
+          {DEBUG ? <span className={styles.debugCoordinates}>{key} : {isPath.toString()}</span> : ''}
           {character}
         </div>
       );
@@ -221,7 +313,7 @@ const TILE_CORNER_ROUNDING = 8;
  */
 function tileBorderRadius(tile: Coordinates, floor: DungeonFloor): string {
   if (!isWalkable(tile, floor)) {
-    return 'none';
+    return '0px';
   }
 
   const left = isWalkable({
